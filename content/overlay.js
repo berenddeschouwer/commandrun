6b77e2b1c0c2cc9e9f439f649cc0bc2680643bd1
aabeling <achimabeling@web.de>
2009-08-10 20:25:32 +0000

    - supports allowed commands per host now - added some exception handling

--- content/overlay.js
+++ content/overlay.js
@@ -2,7 +2,6 @@ var CommandRun = {
   onLoad: function() {
     // initialization code
     this.initialized = true;
-    this.strings = document.getElementById("commandrun-strings");
     var appcontent = document.getElementById("appcontent");
     if (appcontent) {
       appcontent.addEventListener("DOMContentLoaded", CommandRun.onPageLoad, true);
@@ -11,7 +10,7 @@ var CommandRun = {
   onPageLoad: function(event) {
     var win = event.originalTarget.defaultView.wrappedJSObject;
     win.CommandRun = new CommandRunHandler();
-
+	win.CommandRun.page = win.document.location.href;
   }
 };
 window.addEventListener("load", function(e) { CommandRun.onLoad(e); }, false);
@@ -19,37 +18,87 @@ window.addEventListener("load", function(e) { CommandRun.onLoad(e); }, false);
 function CommandRunHandler() {
   this.run = function(command,args) {
     /* check whether command is allowed */
-    if (!this.isCommandAllowed(command)) {
-      // TODO give a better alert with an explanation how to solve
-      alert("command is not allowed: "+command);
+    if (!this.isCommandAllowed(command,this.page)) {
+      var alertText = "Command '"+command+"'\n"
+      	+"is not allowed for page "+this.page+".";
+      alert(alertText);
       return 1;
     }
     var file = Components.classes["@mozilla.org/file/local;1"].
                      createInstance(Components.interfaces.nsILocalFile);
-    file.initWithPath(command);
+    try {
+    	file.initWithPath(command);
+    } catch (e) {
+    	alert("command not found: "+command);
+    	return 1;
+    }
     var blocking = false;
     var process = Components.classes["@mozilla.org/process/util;1"].
                       createInstance(Components.interfaces.nsIProcess);
-    process.init(file);
+    try {
+    	process.init(file);
+    } catch (e) {
+    	alert("command not found: "+command);
+    	return 1;
+    }
 
     var ec = process.run(blocking, args, args.length);
     return ec;
   },
-  this.isCommandAllowed = function(command) {
+  this.isCommandAllowed = function(command,page) {
     /* get the root preferences branch */
     var prefs = Components.classes["@mozilla.org/preferences-service;1"]
                     .getService(Components.interfaces.nsIPrefBranch);
+                    
     /* get the allowed commands preference */
-    var allowedCommandsPref = 
-      prefs.getComplexValue(
-        "extensions.commandrun.allowedcommands",
-        Components.interfaces.nsISupportsString).data;
+    try {
+	    var allowedCommandsPref = 
+	      prefs.getComplexValue(
+	        "extensions.commandrun.allowedcommands",
+	        Components.interfaces.nsISupportsString).data;
+	} catch (NS_ERROR_UNEXPECTED) {
+		/* ignore */
+	}
+	    
     /* evaluate the json text */
-    // TODO replace with more secure JSON parsing 
-    // TODO handle errors
-    var allowedCommands = eval(allowedCommandsPref);
+    if (allowedCommandsPref) {
+    	var allowedCommands = eval(allowedCommandsPref);
+    	if (this.contains(command,allowedCommands)) return true;
+    }
     
-    return (this.contains(command,allowedCommands));
+    /* continue with allowed commands per host preference */
+    try {
+	    var allowedCommandsPerHostPref = 
+	      prefs.getComplexValue(
+	        "extensions.commandrun.allowedcommandsperhost",
+	        Components.interfaces.nsISupportsString).data;
+	} catch (NS_ERROR_UNEXPECTED) {
+		/* ignore */
+	}
+	
+	if (allowedCommandsPerHostPref) {
+		var obj = eval('('+allowedCommandsPerHostPref+')');
+		/* keys of the object are prefixes for pages,
+		   values are lists of commands */
+		try {
+			for (prefix in obj) {
+				if (this.isPrefix(page,prefix)) {
+					/* get allowed commands for this prefix */
+					var allowedCommands = 
+						eval(obj[prefix]);
+    				if (this.contains(command,allowedCommands)) {
+    					return true;
+    				}
+				}		
+			}
+		} catch (e) {
+			alert(
+				"failed to parse extensions.commandrun.allowedcommandsperhost: "
+				+e);
+		}
+	}
+	
+    return false;
   },
   this.contains = function(element,array) {
     var i;
@@ -59,5 +108,12 @@ function CommandRunHandler() {
       }
     }
     return false;
+  },
+  /**
+   * Checks whether the given prefix is a prefix of the
+   * given string.
+   */
+  this.isPrefix = function(string,prefix) {
+  	return (prefix === string.substring(0,prefix.length)); 	
   }
 }
