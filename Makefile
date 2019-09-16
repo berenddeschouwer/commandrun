62523cf44054399b6331144dfcc04e6226a1f857
Berend De Schouwer <berend@deschouwer.co.za>
2019-09-16 15:59:50 +0200

    Add a makefile to build the package
    Add a makefile in preparation to allow setting
    some package-time options

---
+++ Makefile
@@ -0,0 +1,23 @@
+ALLOWED_COMMANDS := /usr/bin/false, /usr/bin/true
+PERMITTED_SITES := .localdomain, server
+
+EXT_RUN := background/background.js content/content.js \
+           options/options.html options/options.js
+PY_RUN := commandrun.py
+
+DOC_FILES := LICENSE README.md
+META_FILES := manifest.json native-messaging-hosts/commandrun.json
+EXAMPLE_FILES := example/example.html example/example.js
+
+all: commandrun.zip
+
+commandrun.zip: $(DOC_FILES) $(META_FILES) $(EXAMPLE_FILES) \
+	        $(EXT_RUN) $(PY_RUN)
+	zip commandrun.zip $(DOC_FILES) $(EXAMPLE_FILES) $(META_FILES) \
+	    $(PY_RUN) $(EXT_RUN)
+
+%.py: %.in.py
+	cp $< $@
+
+%.js: %.in.js
+	cp $< $@
---
+++ commandrun.in.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+# Note that running python with the `-u` flag is required on Windows,
+# in order to ensure that stdin and stdout are opened in binary, rather
+# than text, mode.
+
+import json
+import sys
+import struct
+import syslog
+import os
+import subprocess
+import base64
+
+# Read a message from stdin and decode it.
+def get_message():
+    raw_length = sys.stdin.buffer.read(4)
+    if not raw_length:
+        sys.exit(0)
+    message_length = struct.unpack('=I', raw_length)[0]
+    sys.stderr.write("message length: %d\n" % message_length)
+    message = sys.stdin.buffer.read(message_length)
+    message = message.decode("utf-8")
+    return json.loads(message)
+
+
+# Encode a message for transmission, given its content.
+def encode_message(message_content):
+    sys.stderr.write("message=%s\n" % str(message_content))
+    sys.stderr.flush()
+    encoded_content = json.dumps(message_content).encode("utf-8")
+    encoded_length = struct.pack('=I', len(encoded_content))
+    return {'length': encoded_length, 'content': encoded_content}
+
+
+# Send an encoded message to stdout.
+def send_message(encoded_message):
+    sys.stdout.buffer.write(encoded_message['length'])
+    sys.stdout.buffer.write(encoded_message['content'])
+    sys.stdout.flush()
+
+def send_response(handle, errno, out, err=""):
+    sys.stderr.write("Handle: %d\n" % handle)
+    sys.stderr.write("Errno: %d\n" % errno)
+    sys.stderr.write("Out: %s\n" % out)
+    sys.stderr.flush()
+    response = {}
+    response["handle"] = handle
+    response["errno"] = errno
+    response["stdout"] = base64.b64encode(out).decode("ascii")
+    response["stderr"] = err
+    msg = encode_message(response)
+    send_message(msg)
+
+
+sys.stderr.write("help\n")
+sys.stderr.write("me\n")
+#sys.stderr.flush()
+syslog.syslog("starting")
+sys.stderr.write("logged\n")
+while True:
+    sys.stderr.write("loop\n")
+    message = get_message()
+    sys.stderr.write("got message\n")
+    sys.stderr.write("message: %s\n" % message)
+    sys.stderr.flush()
+    if message["action"] == "run":
+        sys.stderr.write("creating instance\n")
+        sys.stderr.flush()
+        handle = message["handle"]
+        sys.stderr.write("handle of type %s\n" % type(handle))
+        sys.stderr.flush()
+        sys.stderr.write("forking\n")
+        sys.stderr.flush()
+        pid = os.fork()
+        sys.stderr.write("forked\n")
+        sys.stderr.flush()
+        if pid < 0:
+            sys.stderr.write("fork crashed\n")
+            sys.stderr.flush()
+        elif pid > 0: # parent
+            sys.stderr.write("parent\n")
+            sys.stderr.flush()
+            pass
+        else:
+            sys.stderr.write("child\n")
+            sys.stderr.flush()
+            myhandle = handle
+            stufftorun = message["what"]
+            sys.stderr.write("stufftorun: %s\n" % str(stufftorun))
+            sys.stderr.flush()
+            running = subprocess.Popen(stufftorun,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            running.wait()
+            sys.stderr.write("sending done\n")
+            sys.stderr.flush()
+            #send_message(encode_message("done"))
+            send_response(myhandle, running.returncode, running.stdout.read())
+            os._exit(0) # quit forked process
+    else:
+        sys.stderr.write("bad action\n")
+        sys.stderr.flush()
