16a91ef296dfb0ea04c079221400ec1d12a652d7
Berend De Schouwer <berend@deschouwer.co.za>
2019-09-17 14:48:58 +0200

    Start pydoc on commandrun

--- commandrun.in.py
+++ commandrun.in.py
@@ -1,5 +1,51 @@
 #!/usr/bin/python3
 
+"""
+commandrun.py native messaging app
+
+commandrun.py will run commands received from the CommandRun webextension,
+and returns output from those commands back to the browser.
+
+Accepts WebExtension (json) strings on stdin and returns messages
+to the browser on stdout.  WebExtension messages are pre-pended with
+a 4-byte length, native byte order.
+
+Accepted JSON:
+
+    {
+        "action": "run" (string) /* currently only 'run' */
+        "handle": (integer)      /* Messages will be passed back with this
+                                  * handle.  Intended to keep multiple
+                                  * commands separate.
+                                  * >0 - handle
+                                  */
+        "what": (array)          /* which command to run, as passed to execv():
+                                  * [0]   - command name
+                                  * [1]   - first argument
+                                  * [...]
+                                  */
+    }
+
+Returned JSON:
+
+    {
+        "handle": (integer)      /* Matching handle to input
+                                  * <0 - error
+                                  */
+        "errno":  (integer)      /* Errno of command
+                                  * 0  - success
+                                  * >0 - error from command
+                                  * <0 - API error
+                                  *    - -1 forbidden command
+                                  *    - -2 invalid input to native app
+                                  */
+        "stdout": (string)       /* stdout from command */
+        "stderr": (string)       /* stderr from command, or
+                                  * strerror(errno) on error
+                                  */
+    }
+"""
+
 import json
 import sys
 import struct
@@ -9,10 +55,24 @@ import base64
 import signal
 import logging
 
+
+#
+#  We set the logger as a global, so that every function uses the
+#  same one.
+#
 # pylint: disable=invalid-name
 logger = None
 
-def startLogging():
+
+def start_logging():
+    """
+    function
+
+    @returns logging object
+
+    Sets some default logging options.  Some of those options are set
+    according to Makefile variables.
+    """
     this = logging.getLogger('commandrun.py')
     #
     # Set debug according to a Makefile flag.  This is a string and not
@@ -29,17 +89,52 @@ def startLogging():
     this.addHandler(channel)
     return this
 
+
 def wait_for_children(sig, frame):
+    """signal handler
+
+    Simple signal handler to clean up zombie children
+    """
     # pylint: disable=unused-argument
     os.wait()
 
-# Read a message from stdin and decode it.
+
+def send_response(hndl, errno, out, err=""):
+    """function
+
+    @param {int} hndl  - A matching handle
+    @param {int} errno - error number
+    @param {str} out   - stdout
+    @param {str} err   - stderr
+    """
+    logger.debug("Handle=%d", hndl)
+    logger.debug("Errno=%d", errno)
+    logger.debug("Out=%s", out)
+    response = {}
+    response["handle"] = hndl
+    response["errno"] = errno
+    response["stdout"] = base64.b64encode(out).decode("ascii")
+    response["stderr"] = base64.b64encode(err).decode("ascii")
+    msg = encode_message(response)
+    send_message(msg)
+
+
 def get_message():
+    """function
+
+    @returns json
+
+    Read a Native Message WebExtension message from the browser, and
+    return the JSON object.
+    """
     raw_length = sys.stdin.buffer.read(4)
     if not raw_length:
-        sys.exit(0)
+        sys.exit(1)
     message_length = struct.unpack('=I', raw_length)[0]
     logger.debug("message length: %d", message_length)
+    if message_length > 1000000:
+        send_response(-1, -1, "", "message too long")
+        sys.exit(1)
     msg = sys.stdin.buffer.read(message_length)
     msg = msg.decode("utf-8")
     return json.loads(msg)
@@ -59,23 +154,11 @@ def send_message(encoded_message):
     sys.stdout.buffer.write(encoded_message['content'])
     sys.stdout.flush()
 
-def send_response(hndl, errno, out, err=""):
-    logger.debug("Handle=%d", hndl)
-    logger.debug("Errno=%d", errno)
-    logger.debug("Out=%s", out)
-    response = {}
-    response["handle"] = hndl
-    response["errno"] = errno
-    response["stdout"] = base64.b64encode(out).decode("ascii")
-    response["stderr"] = err
-    msg = encode_message(response)
-    send_message(msg)
-
 
 def main():
     # pylint: disable=global-statement
     global logger
-    logger = startLogging()
+    logger = start_logging()
     logger.debug("changing directory")
     os.chdir("/")
     logger.debug("waiting for signals")
@@ -111,6 +194,7 @@ def main():
                 os._exit(0) # pylint: disable=protected-access
         else:
             logger.debug("bad action: %s", message["action"])
+
 
 if __name__ == "__main__":
     main()
