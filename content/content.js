0324ab18498ebd711dd37b028846af68845c7cc1
Berend De Schouwer <berend@deschouwer.co.za>
2019-09-18 16:06:00 +0200

    Better whitespace in content.js

--- content/content.js
+++ content/content.js
@@ -1,29 +1,50 @@
 "use strict";
 
+/*
+ * Content (per-tab) script for CommandRun
+ *
+ * The main extension runs as a background script and as a native app.
+ * This just runs a pass-through sendMessage() from the page to the background
+ * script and back.
+ */
+
 var CommandRun = {
-    run: function(command, output) {
-        console.debug("sending a message");
-        var p = browser.runtime.sendMessage(command);
-        console.debug("sent message, received promise:", p);
-        p.then(
-            /**
-             *  @param {{errno: number, stdout:string, stderr: string}} o
-             */
-        (o) => {
-            if ((typeof o.errno === "undefined") ||
-                (typeof o.stdout === "undefined") ||
-                (typeof o.stderr === "undefined")) {
-                output(-1, "", "incomplete response");
-            } else {
-                output(o.errno, o.stdout, o.stderr);
-            }
-        },
-        () => {
-            console.warn("response to sendMessage() failed promise");
-            output(-1, "", "received bad response");
-        });
-        console.debug("received promise:", p);
-    }
+    run:
+        /**
+         *  @function
+         *  @param {Array} command -   The command to run.  Array[0] is the
+         *                             command, and Array[1..] are parameters
+         *  @param {function(number, string, string)} output -
+         *                             Function to run when the command
+         *                             finishes.
+         */
+        function(command, output) {
+            console.debug("sending a message");
+            var p = browser.runtime.sendMessage(command);
+            console.debug("sent message, received promise:", p);
+            p.then(
+                /**
+                 *  @function
+                 *  @param {{errno: number, stdout:string, stderr: string}} o
+                 *
+                 *  This is the function that gets run when the extension
+                 *  responds with the results of the command.
+                 */
+                (o) => {
+                    if ((typeof o.errno === "undefined") ||
+                        (typeof o.stdout === "undefined") ||
+                        (typeof o.stderr === "undefined")) {
+                        output(-1, "", "incomplete response");
+                    } else {
+                        output(o.errno, o.stdout, o.stderr);
+                    }
+                },
+                () => {
+                    console.warn("response to sendMessage() failed promise");
+                    output(-1, "", "received bad response");
+                });
+            console.debug("received promise:", p);
+        }
 }
 
 
